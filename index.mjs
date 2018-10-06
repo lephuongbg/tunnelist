968594d6ea23fef40fc89eeaa30d31865ff39382
Lê Hoàng Phương <herophuong93@gmail.com>
2018-10-06 17:34:15 +0900

    fix test

--- index.mjs
+++ index.mjs
@@ -3,4 +3,4 @@ import Tunnelist from './lib/tunnelist'
 const host = process.env.TUNNELIST_HOST || '0.0.0.0'
 const port = process.env.TUNNELIST_PORT || '22'
 
-new Tunnelist({host, port}).start()
+new Tunnelist({ host, port }).start()
